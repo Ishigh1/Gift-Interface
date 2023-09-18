2a5f927e54a8365d9ea7959642463e105b6af394
Ishigh <45936615+Ishigh1@users.noreply.github.com>
2023-09-18 22:54:25 +0200

    initial commit

---
+++ GiftHandler.cs
@@ -0,0 +1,61 @@
+﻿using Archipelago.Gifting.Net.Gifts;
+using Archipelago.Gifting.Net.Gifts.Versions.Current;
+using Archipelago.Gifting.Net.Service;
+using Archipelago.Gifting.Net.Traits;
+using Newtonsoft.Json;
+
+namespace Gift_Interface;
+
+public class GiftHandler
+{
+	private readonly GiftingService GiftingService;
+	private const string SavePath = "./gifts.json";
+	public Dictionary<string, (GiftItem item, GiftTrait[] traits)> SavedGifts;
+
+	public GiftHandler(GiftingService giftingService)
+	{
+		GiftingService = giftingService;
+		try
+		{
+			if (File.Exists(SavePath))
+			{
+				// Read the JSON content from the file
+				string jsonContent = File.ReadAllText(SavePath);
+
+				// Deserialize the JSON content into a dictionary
+				SavedGifts =
+					JsonConvert
+						.DeserializeObject<Dictionary<string, (GiftItem item, GiftTrait[] traits)>>(jsonContent)!;
+			}
+		}
+		finally
+		{
+			SavedGifts ??= new Dictionary<string, (GiftItem item, GiftTrait[] traits)>();
+		}
+	}
+
+	public async void OnGiftReceived(Dictionary<string, Gift> _)
+	{
+		Console.WriteLine("Received Gifts : ");
+		Task<Dictionary<string, Gift>> task = GiftingService.GetAllGiftsAndEmptyGiftboxAsync();
+		Dictionary<string, Gift> dictionary = await task;
+		foreach ((string? _, Gift? gift) in dictionary)
+		{
+			Console.WriteLine(
+				$"Received {gift.Amount} {gift.ItemName} (value {gift.ItemValue}) from player {gift.SenderSlot}");
+			for (int index = 0; index < gift.Traits.Length; index++)
+			{
+				GiftTrait giftTrait = gift.Traits[index];
+				Console.WriteLine(
+					$"Trait {index} : {giftTrait.Trait} quality : {giftTrait.Quality} duration : {giftTrait.Duration}");
+			}
+
+			Console.WriteLine();
+
+			GiftItem item = new(gift.ItemName, 1, gift.ItemValue);
+			SavedGifts[item.Name.ToLower()] = (item, gift.Traits);
+		}
+
+		File.WriteAllTextAsync(SavePath, JsonConvert.SerializeObject(SavedGifts));
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Archipelago.Gifting.Net.Gifts;
+using Archipelago.Gifting.Net.Service;
+using Archipelago.Gifting.Net.Traits;
+using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using Gift_Interface;
+
+Console.Write("Enter the server IP address: ");
+string serverIp = Console.ReadLine() ?? throw new InvalidOperationException();
+if (serverIp == "")
+	serverIp = "localhost";
+
+Console.Write("Enter the server port: ");
+int serverPort;
+if (!int.TryParse(Console.ReadLine(), out serverPort))
+{
+	serverPort = 38281;
+}
+
+// Prompt the user for username and password
+Console.Write("Enter your username: ");
+string username = Console.ReadLine() ?? throw new InvalidOperationException();
+if (username == "")
+	username = "Player1";
+
+ArchipelagoSession session = ArchipelagoSessionFactory.CreateSession(serverIp, serverPort);
+session.TryConnectAndLogin("", username, ItemsHandlingFlags.NoItems, tags: new[] { "TextOnly" });
+
+GiftingService giftingService = new(session);
+GiftHandler giftHandler = new(giftingService);
+
+giftingService.OpenGiftBox();
+giftingService.SubscribeToNewGifts(giftHandler.OnGiftReceived);
+
+loop:
+
+string? input = Console.ReadLine(); //Stops at ctrl+z
+if (input == null) goto end;
+
+string[] strings = input.Split(' ');
+switch (strings[0])
+{
+	case "exit":
+		goto end;
+	case "send":
+		string recipient = strings[1];
+		string giftName = string.Join(' ', strings, 2, strings.Length - 2).ToLower();
+		if (giftHandler.SavedGifts.TryGetValue(giftName, out (GiftItem item, GiftTrait[] traits) giftInfo))
+			giftingService.SendGift(giftInfo.item, giftInfo.traits, recipient);
+		else
+			Console.WriteLine("Unknown gift");
+		break;
+	case "list":
+		foreach (string savedGiftsKey in giftHandler.SavedGifts.Keys)
+		{
+			Console.WriteLine(savedGiftsKey);
+		}
+		break;
+}
+
+goto loop;
+
+end: ;
